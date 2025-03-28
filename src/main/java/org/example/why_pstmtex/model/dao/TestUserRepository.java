64ad25e56f3304bd271a6c672b60c5ecad29e3c8
lSNOTNULL <iream1@naver.com>
2025-03-28 16:22:00 +0900

    Hash SHA-256 적용

--- src/main/java/org/example/why_pstmtex/model/dao/TestUserRepository.java
+++ src/main/java/org/example/why_pstmtex/model/dao/TestUserRepository.java
@@ -4,15 +4,22 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.example.why_pstmtex.model.dto.TestUser;
 import org.springframework.stereotype.Repository;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.*;
+import java.util.Base64;
 
 @Repository
 public class TestUserRepository {
     private final Connection connection;
     private final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
+    // 해싱
+    private final MessageDigest digest;
 
-    public TestUserRepository() throws SQLException, ClassNotFoundException {
+
+    public TestUserRepository() throws SQLException, ClassNotFoundException, NoSuchAlgorithmException {
         Class.forName("com.mysql.cj.jdbc.Driver"); // 주의!
         String url = "jdbc:mysql://%s:%s/%s".formatted(
                 dotenv.get("DB_HOST"),
@@ -22,6 +29,8 @@ public class TestUserRepository {
         String username = dotenv.get("DB_USERNAME");
         String password = dotenv.get("DB_PASSWORD");
         connection = DriverManager.getConnection(url, username, password);
+
+        digest = MessageDigest.getInstance("SHA-256");
     }
 
     public void createTestUser(String username, String password) throws SQLException {
@@ -32,7 +41,7 @@ public class TestUserRepository {
         String sql = "INSERT INTO test_user (username, password) VALUES (?, ?)";
         PreparedStatement pstmt = connection.prepareStatement(sql);
         pstmt.setString(1, username);
-        pstmt.setString(2, password);
+        pstmt.setString(2, hashPassword(password));
         pstmt.execute();
     }
 
@@ -44,9 +53,16 @@ public class TestUserRepository {
         String sql = "SELECT * FROM test_user WHERE username ? and password = ?";
         PreparedStatement pstmt = connection.prepareStatement(sql);
         pstmt.setString(1, username);
-        pstmt.setString(2, password);
+        pstmt.setString(2, hashPassword(password));
         ResultSet rs = pstmt.executeQuery();
 
         return rs.next(); // 데이터가 존재하는지 여부
+    }
+
+    private String hashPassword(String password) {
+        byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+//        String hashedPassword = Base64.getEncoder().encodeToString(hash);
+//        return hashedPassword;
+        return Base64.getEncoder().encodeToString(hash);
     }
 }
